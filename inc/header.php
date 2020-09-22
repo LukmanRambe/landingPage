11e3f845c88a6db3bf6dbc38dbc7032e42fb5d72
LukmanRambe <66102026+LukmanRambe@users.noreply.github.com>
2020-09-23 03:12:38 +0700

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Factory</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="fontawesome-free-5.14.0-web/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets/fontawesome-free-5.14.0-web/css/all.min.css">
+    <link rel="stylesheet" href="./assets/styles/style.css">
 </head>
 
 <body>
@@ -22,19 +22,19 @@
                 </button>
 
                 <div>
-                    <img src="image.png" class="img-fluid" alt="brand">
+                    <img src="./assets/images/image.png" class="img-fluid" alt="brand">
                 </div>
 
                 <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo03">
                     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php #hero">Home</a>
+                            <a class="nav-link" href="index.php#hero">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php #features">Features</a>
+                            <a class="nav-link" href="index.php#features">Features</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php #pricing">Pricing</a>
+                            <a class="nav-link" href="index.php#pricing">Pricing</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="contact.php">Contact Us</a>
