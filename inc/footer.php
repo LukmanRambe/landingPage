4d9380602b0815138bf27944da865e961b260ae2
LukmanRambe <66102026+LukmanRambe@users.noreply.github.com>
2020-09-23 03:12:09 +0700

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,39 @@
 <footer>
+    <div class="container">
+        <div class="row">
+            <div class="footer-list col-24">
+                <h6>Menu</h6>
+                <ul>
+                    <li>Features</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+
+            <div class="footer-list col-4">
+                <h6>Our Service</h6>
+                <ul>
+                    <li>Basic</li>
+                    <li>Pro</li>
+                    <li>Premium</li>
+                </ul>
+            </div>
+
+            <div class="footer-list col-4">
+                <h6>Contact Us</h6>
+                <ul>
+                    <li>Jalan Bantan No.37</li>
+                    <li>Kecamatan Medan Tembung, Medan.</li>
+                    <li>Phone : +62 852 6540 2519</li>
+                    <li>E-mail : lukmanrambe1945@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 </footer>
+
 </body>
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
